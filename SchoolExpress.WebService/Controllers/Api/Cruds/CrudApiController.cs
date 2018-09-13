095fee7af299d099d6e36e263f9ba243acf88d6d
mindencoded <illescas.gonzales@gmail.com>
2018-09-12 22:09:50 -0500

    Create QueryableExtension class and remove QueryObject class

--- SchoolExpress.WebService/Controllers/Api/Cruds/CrudApiController.cs
+++ SchoolExpress.WebService/Controllers/Api/Cruds/CrudApiController.cs
@@ -1,17 +1,11 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Data.Entity;
 using System.Dynamic;
 using System.Linq;
-using System.Linq.Dynamic;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Formatting;
 using System.Net.Http.Headers;
-using System.Reflection;
-using System.Text;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Web.Http;
 using Newtonsoft.Json;
@@ -31,10 +25,11 @@ namespace SchoolExpress.WebService.Controllers.Api.Cruds
         [HttpGet]
         public virtual async Task<HttpResponseMessage> Get(int page, int pageSize, string orderBy)
         {
-            QueryResponse queryResponse = await CreateQuery(page, pageSize, orderBy, null, null);
+            IQueryable<T> query = Uow.GetRepositoryForEntityType<T>().GetQueryable().AsNoTracking();
+            dynamic response = await query.CreateQuery(page, pageSize, orderBy, null, null);           
             return new HttpResponseMessage
             {
-                Content = new ObjectContent<QueryResponse>(queryResponse, new JsonMediaTypeFormatter(),
+                Content = new ObjectContent<dynamic>(response, new JsonMediaTypeFormatter(),
                     new MediaTypeHeaderValue("application/json"))
             };
         }
@@ -43,10 +38,11 @@ namespace SchoolExpress.WebService.Controllers.Api.Cruds
         [HttpGet]
         public virtual async Task<HttpResponseMessage> Get(int page, int pageSize, string orderBy, string where)
         {
-            QueryResponse queryResponse = await CreateQuery(page, pageSize, orderBy, where, null);
+            IQueryable<T> query = Uow.GetRepositoryForEntityType<T>().GetQueryable().AsNoTracking();
+            dynamic response = await query.CreateQuery(page, pageSize, orderBy, where, null);  
             return new HttpResponseMessage
             {
-                Content = new ObjectContent<QueryResponse>(queryResponse, new JsonMediaTypeFormatter(),
+                Content = new ObjectContent<dynamic>(response, new JsonMediaTypeFormatter(),
                     new MediaTypeHeaderValue("application/json"))
             };
         }
@@ -56,216 +52,15 @@ namespace SchoolExpress.WebService.Controllers.Api.Cruds
         public virtual async Task<HttpResponseMessage> Get(int page, int pageSize, string orderBy, string where,
             string select)
         {
-            QueryResponse queryResponse = await CreateQuery(page, pageSize, orderBy, where, select);
+            IQueryable<T> query = Uow.GetRepositoryForEntityType<T>().GetQueryable().AsNoTracking();
+            dynamic response = await query.CreateQuery(page, pageSize, orderBy, where, select);  
             return new HttpResponseMessage
             {
-                Content = new ObjectContent<QueryResponse>(queryResponse, new JsonMediaTypeFormatter(),
+                Content = new ObjectContent<dynamic>(response, new JsonMediaTypeFormatter(),
                     new MediaTypeHeaderValue("application/json"))
             };
         }
-
-        private async Task<QueryResponse> CreateQuery(int page, int pageSize, string orderBy, string where,
-            string select)
-        {
-            if (page <= 0)
-            {       
-                throw new ArgumentOutOfRangeException(nameof(page), "page must be greater than 0 (zero)");
-            }
-            /*if (pageSize < page)
-            {
-                throw new ArgumentOutOfRangeException(nameof(pageSize), "pageSize must be greater than page");
-            }*/
-
-            IList<string> orderByList = new List<string>();
-            string[] orderByArray = orderBy.Split(',');
-            foreach (string orderByItem in orderByArray)
-            {
-                if (!string.IsNullOrEmpty(orderByItem))
-                {
-                    if (!orderByItem.EndsWith(" ASC", StringComparison.OrdinalIgnoreCase) &&
-                        !orderByItem.EndsWith(" DESC", StringComparison.OrdinalIgnoreCase))
-                    {
-                        orderByList.Add(orderByItem + " ASC");
-                    }
-                    else
-                    {
-                        orderByList.Add(orderByItem);
-                    }
-                }
-            }
-
-            IQueryable<T> query = Uow.GetRepositoryForEntityType<T>().GetQueryable().AsNoTracking();
-                         if (!string.IsNullOrEmpty(where))
-                         {
-                int countParam = 0;
-                IList<object> parameters = new List<object>();
-                IList<string> andExpressionsList = new List<string>();
-
-                string[] andExpressions = Regex.Split(where, "AND", RegexOptions.IgnoreCase);
-                foreach (string andExpression in andExpressions)
-                {
-                    string[] orExpressions = Regex.Split(andExpression, "OR", RegexOptions.IgnoreCase);
-                    if (orExpressions.Length > 1)
-                    {
-                        IList<string> orExpressionsList = new List<string>();
-                        foreach (string orExpression in orExpressions)
-                        {
-                            object value;
-                            string expression = BuildExpression(orExpression, countParam, out value);
-                            if (expression != null)
-                            {
-                                orExpressionsList.Add(expression);
-                                parameters.Add(value);
-                                countParam++;
-                            }
-                        }
-
-                        andExpressionsList.Add(string.Join(" OR ", orExpressionsList.ToArray()));
-                    }
-                    else
-                    {
-                        object value;
-                        string expression = BuildExpression(andExpression, countParam, out value);
-                        if (expression != null)
-                        {
-                            andExpressionsList.Add(expression);
-                            parameters.Add(value);
-                            countParam++;
-                        }
-                    }
-                }
-
-                query = query.Where(string.Join(" AND ", andExpressionsList.ToArray()), parameters.ToArray());
-            }
-
-            int total = await query.CountAsync();
-           
-            
-            query = query.OrderBy(string.Join(",", orderByList.ToArray())).Skip((page - 1) * pageSize).Take(pageSize);
-            
-
-            IList<dynamic> result;
-            if (!string.IsNullOrEmpty(select))
-            {
-                result = await query.Select(new StringBuilder().Append("new (").Append(select).Append(")").ToString())
-                    .ToListAsync();
-            }
-            else
-            {
-                result = await query.ToListAsync<dynamic>();
-            }
-
-            int count = result.Count;
-            int pages = CalculateTotalPages(total, pageSize);
-            return new QueryResponse
-            {
-                Value = result,
-                Count = count,
-                Total = total,
-                Pages = pages,
-                Page = page,
-                PageSize = pageSize
-            };
-        }
-
-        public string BuildExpression(string expression, int count, out object value)
-        {
-            value = null;
-            foreach (string symbol in RelationalOperatorExtension.Symbols)
-            {
-                string[] parts = expression.Split(new[] {symbol}, StringSplitOptions.None).Select(p => p.Trim())
-                    .ToArray();
-                if (parts.Length > 1)
-                {
-                    string property = parts[0];
-                    string openParenthesis = "";
-                    while (property.StartsWith("("))
-                    {
-                        property = property.Substring(1);
-                        openParenthesis += "(";
-                    }
-
-                    string strValue = parts[1];
-                    string closeParenthesis = "";
-                    while (strValue.EndsWith(")"))
-                    {
-                        strValue = strValue.Remove(strValue.Length - 1);
-                        closeParenthesis += ")";
-                    }
-
-                    RelationalOperator relationalOperator = RelationalOperatorExtension.GetOperator(symbol);
-                    if (relationalOperator == RelationalOperator.Contains)
-                    {
-                        string[] cleanValues = Regex.Replace(strValue, @"^(\[|\(){1}(.*?)(\]|\)){1}$", "$2")
-                            .Split(',').Select(p => p.Trim('\'', ' ')).ToArray();
-
-                        IList listValues;
-                        PropertyInfo propertyInfo = typeof(T).GetProperty(property);
-                        if (propertyInfo != null)
-                        {
-                            listValues =
-                                (IList) Activator.CreateInstance(
-                                    typeof(List<>).MakeGenericType(propertyInfo.PropertyType));
-                            foreach (string cleanValue in cleanValues)
-                            {
-                                listValues.Add(Convert.ChangeType(cleanValue, propertyInfo.PropertyType));
-                            }
-
-                            value = listValues;
-                        }
-                        else
-                        {
-                            listValues = new List<string>();
-                            foreach (string cleanValue in cleanValues)
-                            {
-                                listValues.Add(cleanValue);
-                            }
-
-                            value = listValues;
-                        }
-
-                        return new StringBuilder().Append(openParenthesis).Append("@").Append(count)
-                            .Append(".Contains(outerIt.").Append(property).Append(")").Append(closeParenthesis)
-                            .ToString();
-
-                    }
-                    else
-                    {
-                        string cleanValue = strValue.Trim('\'', ' ');
-                        PropertyInfo propertyInfo = typeof(T).GetProperty(property);
-                        if (propertyInfo != null)
-                        {
-                            value = Convert.ChangeType(cleanValue, propertyInfo.PropertyType);
-                        }
-                        else
-                        {
-                            value = cleanValue;
-                        }
-
-                        if (relationalOperator == RelationalOperator.Like ||
-                            relationalOperator == RelationalOperator.StartsWith)
-                        {
-                            string method = "Contains";
-                            if (relationalOperator == RelationalOperator.StartsWith)
-                            {
-                                method = "StartsWith";
-                            }
-
-                            return new StringBuilder().Append(openParenthesis).Append("it.").Append(property)
-                                .Append(".").Append(method).Append("(@").Append(count).Append(")")
-                                .Append(closeParenthesis)
-                                .ToString();
-                        }
-
-                        return new StringBuilder().Append(openParenthesis).Append(property).Append(" ")
-                            .Append(symbol).Append(" ").Append("@" + count).Append(closeParenthesis).ToString();
-                    }
-                }
-            }
-
-            return null;
-        }
-
+        
         [Route("{id}")]
         [HttpGet]
         public virtual async Task<HttpResponseMessage> Get(object id)
@@ -329,18 +124,6 @@ namespace SchoolExpress.WebService.Controllers.Api.Cruds
             Uow.GetRepositoryForEntityType<T>().Delete(id);
             await Uow.CommitAsync();
             return new HttpResponseMessage(HttpStatusCode.NoContent);
-        }
-
-        private int CalculateTotalPages(int totalItems, int itemsPerPage)
-        {
-            int totalPages = totalItems / itemsPerPage;
-
-            if (totalItems % itemsPerPage != 0)
-            {
-                totalPages++; // Last page with only 1 item left
-            }
-
-            return totalPages;
         }
     }
 }
--- SchoolExpress.WebService/Utils/QueryableExtensions.cs
+++ SchoolExpress.WebService/Utils/QueryableExtensions.cs
@@ -1,7 +1,233 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Dynamic;
+using System.Linq;
+using System.Linq.Dynamic;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
 namespace SchoolExpress.WebService.Utils
 {
-    public class QueryableExtensions
+    public static class QueryableExtensions
     {
-        
+        public static async Task<dynamic> CreateQuery<T>(this IQueryable<T> query, int page, int pageSize,
+            string orderBy, string where,
+            string select) where T : class
+        {
+            if (page <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(page), "page must be greater than 0.");
+            }
+            /*if (pageSize < page)
+            {
+                throw new ArgumentOutOfRangeException(nameof(pageSize), "pageSize must be greater than page.");
+            }*/
+
+            IList<string> orderByList = new List<string>();
+            string[] orderByArray = orderBy.Split(',');
+            foreach (string orderByItem in orderByArray)
+            {
+                if (!string.IsNullOrEmpty(orderByItem))
+                {
+                    if (!orderByItem.EndsWith(" ASC", StringComparison.OrdinalIgnoreCase) &&
+                        !orderByItem.EndsWith(" DESC", StringComparison.OrdinalIgnoreCase))
+                    {
+                        orderByList.Add(orderByItem + " ASC");
+                    }
+                    else
+                    {
+                        orderByList.Add(orderByItem);
+                    }
+                }
+            }
+
+
+            if (!string.IsNullOrEmpty(where))
+            {
+                int countParam = 0;
+                IList<object> parameters = new List<object>();
+                IList<string> andExpressionsList = new List<string>();
+
+                string[] andExpressions = Regex.Split(where, "AND", RegexOptions.IgnoreCase);
+                foreach (string andExpression in andExpressions)
+                {
+                    string[] orExpressions = Regex.Split(andExpression, "OR", RegexOptions.IgnoreCase);
+                    if (orExpressions.Length > 1)
+                    {
+                        IList<string> orExpressionsList = new List<string>();
+                        foreach (string orExpression in orExpressions)
+                        {
+                            object value;
+                            string expression = BuildExpression<T>(orExpression, countParam, out value);
+                            if (expression != null)
+                            {
+                                orExpressionsList.Add(expression);
+                                parameters.Add(value);
+                                countParam++;
+                            }
+                        }
+
+                        andExpressionsList.Add(string.Join(" OR ", orExpressionsList.ToArray()));
+                    }
+                    else
+                    {
+                        object value;
+                        string expression = BuildExpression<T>(andExpression, countParam, out value);
+                        if (expression != null)
+                        {
+                            andExpressionsList.Add(expression);
+                            parameters.Add(value);
+                            countParam++;
+                        }
+                    }
+                }
+
+                query = query.Where(string.Join(" AND ", andExpressionsList.ToArray()), parameters.ToArray());
+            }
+
+            int total = await query.CountAsync();
+
+
+            query = query.OrderBy(string.Join(",", orderByList.ToArray())).Skip((page - 1) * pageSize).Take(pageSize);
+
+
+            IList<dynamic> result;
+            if (!string.IsNullOrEmpty(select))
+            {
+                result = await query.Select(new StringBuilder().Append("new (").Append(select).Append(")").ToString())
+                    .ToListAsync();
+            }
+            else
+            {
+                result = await query.ToListAsync<dynamic>();
+            }
+
+            int count = result.Count;
+            int pages = CalculateTotalPages(total, pageSize);
+            dynamic expandoObject = new ExpandoObject();
+            expandoObject.Value = result;
+            expandoObject.Count = count;
+            expandoObject.Total = total;
+            expandoObject.Pages = pages;
+            expandoObject.Page = page;
+            expandoObject.PageSize = pageSize;
+            return expandoObject;
+        }
+
+        private static string BuildExpression<T>(string expression, int count, out object value) where T : class
+        {
+            value = null;
+            foreach (string symbol in RelationalOperatorExtension.Symbols)
+            {
+                string[] parts = expression.Split(new[] {symbol}, StringSplitOptions.None).Select(p => p.Trim())
+                    .ToArray();
+                if (parts.Length > 1)
+                {
+                    string property = parts[0];
+                    string openParenthesis = "";
+                    while (property.StartsWith("("))
+                    {
+                        property = property.Substring(1);
+                        openParenthesis += "(";
+                    }
+
+                    string strValue = parts[1];
+                    string closeParenthesis = "";
+                    while (strValue.EndsWith(")"))
+                    {
+                        strValue = strValue.Remove(strValue.Length - 1);
+                        closeParenthesis += ")";
+                    }
+
+                    RelationalOperator relationalOperator = RelationalOperatorExtension.GetOperator(symbol);
+                    if (relationalOperator == RelationalOperator.Contains)
+                    {
+                        string[] cleanValues = Regex.Replace(strValue, @"^(\[|\(){1}(.*?)(\]|\)){1}$", "$2")
+                            .Split(',').Select(p => p.Trim('\'', ' ')).ToArray();
+
+                        IList listValues;
+                        PropertyInfo propertyInfo = typeof(T).GetProperty(property);
+                        if (propertyInfo != null)
+                        {
+                            listValues =
+                                (IList) Activator.CreateInstance(
+                                    typeof(List<>).MakeGenericType(propertyInfo.PropertyType));
+                            foreach (string cleanValue in cleanValues)
+                            {
+                                listValues.Add(Convert.ChangeType(cleanValue, propertyInfo.PropertyType));
+                            }
+
+                            value = listValues;
+                        }
+                        else
+                        {
+                            listValues = new List<string>();
+                            foreach (string cleanValue in cleanValues)
+                            {
+                                listValues.Add(cleanValue);
+                            }
+
+                            value = listValues;
+                        }
+
+                        return new StringBuilder().Append(openParenthesis).Append("@").Append(count)
+                            .Append(".Contains(outerIt.").Append(property).Append(")").Append(closeParenthesis)
+                            .ToString();
+
+                    }
+                    else
+                    {
+                        string cleanValue = strValue.Trim('\'', ' ');
+                        PropertyInfo propertyInfo = typeof(T).GetProperty(property);
+                        if (propertyInfo != null)
+                        {
+                            value = Convert.ChangeType(cleanValue, propertyInfo.PropertyType);
+                        }
+                        else
+                        {
+                            value = cleanValue;
+                        }
+
+                        if (relationalOperator == RelationalOperator.Like ||
+                            relationalOperator == RelationalOperator.StartsWith)
+                        {
+                            string method = "Contains";
+                            if (relationalOperator == RelationalOperator.StartsWith)
+                            {
+                                method = "StartsWith";
+                            }
+
+                            return new StringBuilder().Append(openParenthesis).Append("it.").Append(property)
+                                .Append(".").Append(method).Append("(@").Append(count).Append(")")
+                                .Append(closeParenthesis)
+                                .ToString();
+                        }
+
+                        return new StringBuilder().Append(openParenthesis).Append(property).Append(" ")
+                            .Append(symbol).Append(" ").Append("@" + count).Append(closeParenthesis).ToString();
+                    }
+                }
+            }
+
+            return null;
+        }
+
+
+        private static int CalculateTotalPages(int totalItems, int itemsPerPage)
+        {
+            int totalPages = totalItems / itemsPerPage;
+
+            if (totalItems % itemsPerPage != 0)
+            {
+                totalPages++; // Last page with only 1 item left
+            }
+
+            return totalPages;
+        }
+
     }
 }
